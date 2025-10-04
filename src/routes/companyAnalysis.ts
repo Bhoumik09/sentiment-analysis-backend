1518b46eb47df4caecb24facc080eb3e0a0a77af
Bhoumik <Bhoumikchopra12@gmail.com>
2025-10-04 07:57:27 +0530

    added secured middlewares

--- src/routes/companyAnalysis.ts
+++ src/routes/companyAnalysis.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import { verifyTokenLogin } from '../middlewares/authMiddleware';
 import { companyAnalysisTrend,  companyInformation,  companySentimentInfo, recentNewsOfCompany, sentimentTrendOverPeriod } from '../controllers/companyAnalysisController';
 const companyRouter = express.Router();
-companyRouter.get('/:companyId',companyInformation);
-companyRouter.get('/overview/:companyId',companySentimentInfo);
-companyRouter.get('/recent-news/:companyId',recentNewsOfCompany);
-companyRouter.get('/sentiment-trend/:companyId',sentimentTrendOverPeriod);
-companyRouter.get('/company-analysis-trend/:companyId',companyAnalysisTrend)
+companyRouter.get('/:companyId',verifyTokenLogin,companyInformation);
+companyRouter.get('/overview/:companyId',verifyTokenLogin,companySentimentInfo);
+companyRouter.get('/recent-news/:companyId',verifyTokenLogin,recentNewsOfCompany);
+companyRouter.get('/sentiment-trend/:companyId',verifyTokenLogin,sentimentTrendOverPeriod);
+companyRouter.get('/company-analysis-trend/:companyId',verifyTokenLogin,companyAnalysisTrend)
 export default companyRouter;
--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -2,5 +2,5 @@ import express from 'express'
 import { verifyTokenLogin } from '../middlewares/authMiddleware';
 import { search } from '../controllers/searchController';
 const searchRouter = express.Router();
-searchRouter.get('/fetch-data', search)
+searchRouter.get('/fetch-data',verifyTokenLogin, search)
 export default searchRouter;
