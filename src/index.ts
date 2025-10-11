14c15866dcd2dadd4ecea72fac999f1554cefe89
Bhoumik <Bhoumikchopra12@gmail.com>
2025-10-11 12:20:46 +0530

    build added

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-import express from 'express'
-import authRouter from './routes/authRoute';
-import { httpLogger } from './middlewares/httpLogger';
-import dashboardRouter from './routes/dashboardRoutes';
-import searchRouter from './routes/searchRoutes';
-import companyRouter from './routes/companyAnalysis';
-const app= express();
-app.use(httpLogger)
-app.use(express.urlencoded({extended:true}));
+import express from "express";
+import authRouter from "./routes/authRoute";
+import { httpLogger } from "./middlewares/httpLogger";
+import dashboardRouter from "./routes/dashboardRoutes";
+import searchRouter from "./routes/searchRoutes";
+import companyRouter from "./routes/companyAnalysis";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+app.use(httpLogger);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/auth",authRouter);
-app.use('/dashboard', dashboardRouter);
-app.use('/searchQuery',searchRouter);
-app.use('/company', companyRouter);
+app.use("/auth", authRouter);
+app.use("/dashboard", dashboardRouter);
+app.use("/searchQuery", searchRouter);
+app.use("/company", companyRouter);
 // add()
-app.listen(5000,()=>{
-    console.log("Server is running at 5000")
-})
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  app.listen(5000, () => {
+    console.log("Server is running at 5000");
+  });
+}
+export default app;
